9c74c920bda199cf31bf58d9f83fab6459e7291a
Quentin ANIERE <quentn.aniere@gmail.com>
2023-04-04 02:01:31 -0400

    WIP

--- backend/src/main/java/com/coiffex/backend/HelloWorldController.java
+++ backend/src/main/java/com/coiffex/backend/HelloWorldController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldContreller {
+public class HelloWorldController {
     
     @GetMapping("/")
     public String hello() {
