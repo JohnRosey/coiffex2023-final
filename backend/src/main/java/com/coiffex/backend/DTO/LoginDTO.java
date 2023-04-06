3492c3a3a2cfebafe654b8938e9d66e04a6ea9eb
Quentin ANIERE <quentin.aniere@gmail.com>
2023-04-05 21:08:58 -0400

    WIP

---
+++ backend/src/main/java/com/coiffex/backend/DTO/LoginDTO.java
@@ -0,0 +1,10 @@
+package com.coiffex.backend.DTO;
+
+import lombok.Data;
+
+
+@Data
+public class LoginDTO {
+  private String username = "";
+  private String password = "";
+}
--- backend/src/main/java/com/coiffex/backend/controllers/UserController.java
+++ backend/src/main/java/com/coiffex/backend/controllers/UserController.java
@@ -1,22 +1,28 @@
 package com.coiffex.backend.controllers;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coiffex.backend.models.User;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
+
     @ResponseBody
-    @PostMapping("/user/login")
-    public String login(User user) {
-        return "hello";
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) {
+        return "Username: " + username + " Password: " + password;
     }
 
     @ResponseBody
-    @PostMapping("/user/register")
+    @PostMapping("/register")
     public String register(User user) {
         return user.getName();
     }
