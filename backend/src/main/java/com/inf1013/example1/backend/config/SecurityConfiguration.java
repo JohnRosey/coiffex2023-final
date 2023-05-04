78b3ec9454652c1485589baf97578b3b4347373b
Mouhamed DIOP <diopmouh@uqtr.ca>
2023-05-03 20:15:00 -0400

    UserService

--- backend/src/main/java/com/inf1013/example1/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/inf1013/example1/backend/config/SecurityConfiguration.java
@@ -1,19 +1,8 @@
 package com.inf1013.example1.backend.config;
 
-  package com.dailycodebuffer.jwt.config;
 
-import com.dailycodebuffer.jwt.filter.JwtFilter;
-import com.dailycodebuffer.jwt.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
   @Configuration
   @EnableWebSecurity
